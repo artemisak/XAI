55efe5c0700e078c9af8e5861bdc2d3ace767435
Artem Isakov <76273674+artemisak@users.noreply.github.com>
2024-06-30 12:19:23 +0300

    new visualization added

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template, request, jsonify
 import anthropic
 import ast
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import os
+import plotly.graph_objects as go
+import plotly.utils
+from statistics import mean, stdev, variance
+import numpy as np
 
 app = Flask(__name__)
 
@@ -31,7 +35,13 @@ def replace_with_mask(text, word_dict, key_word):
     masked_text = ' '.join(masked_words).replace(' ,', ',').replace(' .', '.')
     return masked_text
 
+def text_preprocess(text1, text2):
+    text1 = text1.replace(',', '').replace('.', '')
+    text2 = text2.replace(',', '').replace('.', '')
+    return text1, text2
+
 def get_jaccard_similarity(text1, text2):
+    text1 , text2 = text_preprocess(text1, text2)
     set1 = set(text1.split())
     set2 = set(text2.split())
     intersection = len(set1 & set2)
@@ -39,11 +49,13 @@ def get_jaccard_similarity(text1, text2):
     return intersection / union
 
 def get_cosine_similarity(text1, text2):
-    vectorizer = CountVectorizer().fit_transform([text1, text2])
-    vectors = vectorizer.toarray()
-    return cosine_similarity(vectors)[0][1]
+    text1 , text2 = text_preprocess(text1, text2)
+    vectorizer = TfidfVectorizer()
+    tfidf_matrix = vectorizer.fit_transform([text1, text2])
+    return cosine_similarity(tfidf_matrix[0:1], tfidf_matrix[1:2]).item()
 
 def get_levenshtein_distance(text1, text2):
+    text1 , text2 = text_preprocess(text1, text2)
     m, n = len(text1), len(text2)
     dp = [[0] * (n + 1) for _ in range(m + 1)]
 
@@ -61,6 +73,51 @@ def get_levenshtein_distance(text1, text2):
 
     return 1 - dp[m][n] / max(m, n)
 
+def create_horizontal_bar_plot(concepts, distances, name, xtitle, ytitle='Concepts'):
+    
+    # Create a list of tuples (concept, distance) and sort it
+    sorted_data = sorted(zip(concepts, distances), key=lambda x: x[1], reverse=False)
+    
+    # Unzip the sorted data
+    sorted_concepts, sorted_distances = zip(*sorted_data)
+
+    fig = go.Figure(go.Bar(
+        y=sorted_concepts,  # Use sorted concepts
+        x=sorted_distances,  # Use sorted distances
+        orientation='h',
+        marker_color='rgba(50, 171, 96, 0.6)',
+        text=[f'{d:.4f}' for d in sorted_distances],
+        textposition='inside'
+    ))
+    fig.update_layout(
+        title=name,
+        xaxis_title=xtitle,
+        yaxis_title=ytitle,
+        height=400,
+        margin=dict(l=0, r=0, t=30, b=0)
+    )
+    return plotly.utils.PlotlyJSONEncoder().encode(fig)
+
+def make_a_request(text):
+    message = client.messages.create(
+        model="claude-3-5-sonnet-20240620",
+        max_tokens=1000,
+        temperature=0,
+        system='Provide your answers without any explanation or additional text.',
+        messages=[
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": text
+                    }
+                ]
+            }
+        ]
+    )
+    return message.content[0].text
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -71,50 +128,44 @@ def process():
     scheme = request.form['scheme']
 
     # Extract concepts
-    message_2 = client.messages.create(
-        model="claude-3-opus-20240229",
-        max_tokens=1000,
-        temperature=0,
-        system="Make your answers clear and concise.",
-        messages=[
-            {
-                "role": "user",
-                "content": [
-                    {
-                        "type": "text",
-                        "text": f"Extract up to 5 key concepts from the text below, give an answer in the form of a comma-separated list, each concept should consist of one or two words or be a named entity: {prompt}"
-                    }
-                ]
-            }
-        ]
-    )
-
-    concepts = message_2.content[0].text.split(', ')
+    concepts = make_a_request(f"Analyze the following text and identify up to five key concepts. Present your answer as a comma-separated list. Each concept should be expressed in one or two words or be a specific named entity. Focus on the most essential ideas, terms, or themes: {prompt}").split(', ')
 
     # Relate concepts to words
-    message_3 = client.messages.create(
-        model="claude-3-opus-20240229",
-        max_tokens=1000,
-        temperature=0,
-        system="Make your answers clear and concise.",
-        messages=[
-            {
-                "role": "user",
-                "content": [
-                    {
-                        "type": "text",
-                        "text": f"Relate these key concepts to the words in the text. Provide the answer in the form of a python dictionary, with the key being the concept and the value being a list of words that are directly or conventionally related to the concept. Key concepts: {concepts}. Text: {prompt}."
-                    }
-                ]
-            }
-        ]
-    )
-
-    concepts_with_words = split_words_in_dict(ast.literal_eval(message_3.content[0].text))
+    concepts_with_words = split_words_in_dict(ast.literal_eval(make_a_request(f"Analyze the text and identify words related to each key concept. Provide the output as a Python dictionary. Each key is one of the given concepts. The corresponding value is a list of words or phrases from the text that are closely related to or exemplify that concept. Key concepts: {concepts}. Text to analyze: {prompt}. Include only words and phrases that appear in the given text. If a concept has no related words in the text, use an empty list as its value. Ensure the dictionary is properly formatted and can be directly executed as Python code.")))
 
     return jsonify({
         'concepts': concepts,
         'concepts_with_words': concepts_with_words
+    })
+
+@app.route('/visualize_concepts', methods=['POST'])
+def visualize_concepts():
+    prompt = request.form['prompt']
+    scheme = request.form['scheme']
+    concepts = request.form.getlist('concepts[]')
+    concepts_with_words = ast.literal_eval(request.form['concepts_with_words'])
+
+    # Calculate concept-wise distances
+    cosine_similarities = []
+    original_answer = make_a_request(f"{prompt}{scheme}")
+    for concept in concepts:
+        bootstrap_samples = []
+        masked_prompt = replace_with_mask(prompt, concepts_with_words, concept)
+        for i in range(10):
+            filled_prompt = make_a_request(f'Assess your degree of confidence in what to put in masked fields. If you think there is more than one appropriate choice, choose a generalizing form. Start filling in the words at the end. Use only common lexicon. Provide only the filled text without any additional explanations. Text to analyze: {masked_prompt}')
+            masked_answer = make_a_request(f"{filled_prompt}{scheme}")
+            bootstrap_samples.append(get_cosine_similarity(original_answer, masked_answer))
+        cosine_similarities.append(bootstrap_samples)
+    cosine_similarities_mean = [mean(sample) for sample in cosine_similarities]
+    cosine_similarities_variance = [variance(sample) for sample in cosine_similarities]
+
+    # Create horizontal bar plot
+    concept_mean = create_horizontal_bar_plot(concepts, cosine_similarities_mean, 'Concept impact on the output', 'Mean cosine similarity over a few shot')
+    concept_variance = create_horizontal_bar_plot(concepts, cosine_similarities_variance, 'Estimated variability of the given output', 'Cosine similarity variance over a few shot')
+
+    return jsonify({
+        'concept_mean': concept_mean,
+        'concept_variance': concept_variance
     })
 
 @app.route('/mask', methods=['POST'])
@@ -125,51 +176,15 @@ def mask():
     concepts_with_words = ast.literal_eval(request.form['concepts_with_words'])
 
     # Original answer
-    message_1 = client.messages.create(
-        model="claude-3-opus-20240229",
-        max_tokens=1000,
-        temperature=0,
-        system="Make your answers clear and concise.",
-        messages=[
-            {
-                "role": "user",
-                "content": [
-                    {
-                        "type": "text",
-                        "text": f"{prompt}{scheme}"
-                    }
-                ]
-            }
-        ]
-    )
-
-    original_answer = message_1.content[0].text
+    original_answer = make_a_request(f"{prompt}{scheme}")
 
     # Masked prompt
-    mask_prompt = prompt
+    masked_prompt = prompt
     for concept in concepts:
-        mask_prompt = replace_with_mask(mask_prompt, concepts_with_words, concept)
+        masked_prompt = replace_with_mask(masked_prompt, concepts_with_words, concept)
 
     # Masked answer
-    message_4 = client.messages.create(
-        model="claude-3-opus-20240229",
-        max_tokens=1000,
-        temperature=0,
-        system="Make your answers clear and concise.",
-        messages=[
-            {
-                "role": "user",
-                "content": [
-                    {
-                        "type": "text",
-                        "text": f"{mask_prompt}{scheme}"
-                    }
-                ]
-            }
-        ]
-    )
-
-    masked_answer = message_4.content[0].text
+    masked_answer = make_a_request(f"{masked_prompt}{scheme}")
 
     # Calculate similarities
     jaccard = get_jaccard_similarity(original_answer, masked_answer)
@@ -178,7 +193,7 @@ def mask():
 
     return jsonify({
         'original_prompt': prompt,
-        'masked_prompt': mask_prompt,
+        'masked_prompt': masked_prompt,
         'original_answer': original_answer,
         'masked_answer': masked_answer,
         'jaccard': jaccard,
